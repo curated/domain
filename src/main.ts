0d8bffcf557486c8809abe9dd30bf08c8b309972
Wagner Camarao <wagner@camarao.io>
2018-03-09 22:49:31 -0300

    Fix typo

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const connectionPromise = createConnection({
   type: 'postgres',
   port: 5432,
   host: process.env.PG_HOST,
-  username: process.env.PG_HOST,
+  username: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
   logging: !Object.is('test', process.env.NODE_ENV),
